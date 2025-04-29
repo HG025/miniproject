6ab0836ecea4f4e39bbd13d838636299b5b3bbcb
HG025 <huzaifagujjar025@gmail.com>
2025-04-29 22:56:28 +0500

    Kendo UI and Angular Test ready

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { DateInputsModule, KENDO_DATEINPUTS } from '@progress/kendo-angular-dateinputs';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, KENDO_DATEINPUTS],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
   title = 'miniproject';
 }
